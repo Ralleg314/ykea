42b853a8b299fc58a894c4adaa9ff299d0c835fa
Ralleg314 <ralleg314@gmail.com>
2018-06-05 23:58:31 +0200

    com

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -75,48 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PracticaWeb.wsgi.application'
 
-#CORS
-CORS_URLS_REGEX = '/api/.*$'
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-
-CORS_ORIGIN_WHITELIST = (
-    'sd2018-ykea-a1.herokuapp.com',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
@@ -196,6 +154,51 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/ykea/accounts/login'
 LOGIN_REDIRECT_URL='/ykea'
 LOGOUT_REDIRECT_URL='/ykea'
+
+#CORS
+CORS_URLS_REGEX = '/api/.*$'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
+CORS_ORIGIN_WHITELIST = (
+    'sd2018-ykea-a1.herokuapp.com',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
