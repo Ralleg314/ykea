99ee1e0dec4e955c89b9d3d8f4daee8dbeb7ab78
Ralleg314 <ralleg314@gmail.com>
2018-06-05 18:23:23 +0200

    com

--- ykea/models.py
+++ ykea/models.py
@@ -43,11 +43,6 @@ class Client(models.Model):
     money = models.DecimalField(max_digits=12, decimal_places=2)
     def __str__(self):
         return (self.user.username +" " + str(self.money))
-    
-class Comercial(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return (self.user.username)
 
 class ShoppingCart(models.Model):
     #id = models.CharField(max_length=8, unique=True)
