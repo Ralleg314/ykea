14c1e267bb273ff482912ab28400c151261f31ea
Ralleg314 <ralleg314@gmail.com>
2018-06-05 23:39:36 +0200

    com

--- ykea/apps.py
+++ ykea/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class YkeaConfig(AppConfig):
     name = 'ykea'
+	
 	def ready(self):
         # Makes sure all signal handlers are connected
         from . import handlers  # noqa
