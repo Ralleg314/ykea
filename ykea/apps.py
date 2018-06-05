0213db2d3090b27dde6f22f97c8ed6b4155735aa
Ralleg314 <ralleg314@gmail.com>
2018-06-05 23:36:58 +0200

    com

--- ykea/apps.py
+++ ykea/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class YkeaConfig(AppConfig):
     name = 'ykea'
+	def ready(self):
+        # Makes sure all signal handlers are connected
+        from . import handlers  # noqa
--- ykea/permissions.py
+++ ykea/permissions.py
@@ -1,10 +1,5 @@
 from rest_framework import permissions
-from corsheaders.signals import check_request_enabled
 
-def cors_allow_api_to_everyone(sender, request, **kwargs):
-    return request.path.startswith('/api/')
-
-check_request_enabled.connect(cors_allow_api_to_everyone)
 
 class IsCommercialOrReadOnly(permissions.BasePermission):
     """
